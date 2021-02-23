cf0319a77542bdac1117675092121c1a801361d6
Minh Ngo <ngo.minh0224@gmail.com>
2021-02-22 23:18:45 -0800

    pokemon detail

--- src/App.js
+++ src/App.js
@@ -1,33 +1,34 @@
-import {
-    BrowserRouter as Router, 
-    Route, 
-    Switch,
-  } from 'react-router-dom';
-  import SearchPage from './SearchPage.js'
-  import './App.css';
-  import Header from './Header.js'
-  import HomePage from './HomePage'
-  
-  function App() {
-    return (
-      <div className="App">
-           <Router>
-             <Header/>
-                      <Switch>
-                          <Route 
-                              path="/" 
-                              exact
-                              render={(routerProps) => <HomePage {...routerProps} />} 
-                          />
-                          <Route 
-                              path="/search" 
-                              exact
-                              render={(routerProps) => <SearchPage {...routerProps} />} 
-                          />
-                      </Switch>
-                  </Router>
-      </div>
-    );
-  }
-  
-  export default App;
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import SearchPage from './SearchPage.js';
+import './App.css';
+import Header from './Header.js';
+import HomePage from './HomePage';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Switch>
+          <Route
+            path="/"
+            exact
+            render={(routerProps) => <HomePage {...routerProps} />}
+          />
+          <Route
+            path="/search"
+            exact
+            render={(routerProps) => <SearchPage {...routerProps} />}
+          />
+          <Route
+            path="/pokemon/:id"
+            exact
+            render={(routerProps) => <PokemonDetail {...routerProps} />}
+          />
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
